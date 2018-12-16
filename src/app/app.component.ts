ecef932713823568a28e979be042acd919044963
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2018-12-16 23:17:04 +0200

    bootstrap + fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bitmexcallbot-ui';
+  hidden1 = false;
+  hidden2 = false;
+  hidden3 = false;
+  faCoffee = faCoffee;
+
+  hideOrAppear1() {
+    this.hidden1 = !this.hidden1;
+  }
+
+  hideOrAppear2() {
+    this.hidden2 = !this.hidden2;
+  }
+
+  hideOrAppear3() {
+    this.hidden3 = !this.hidden3;
+  }
 }
