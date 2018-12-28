e4f81b04490607367c9f9d44dcd56ca3e25e87cd
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2018-12-28 16:21:47 +0200

    split navbar into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,4 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faHandshake } from '@fortawesome/free-solid-svg-icons';
-import { faHistory } from '@fortawesome/free-solid-svg-icons';
-import { faWallet } from '@fortawesome/free-solid-svg-icons';
-import { faCogs } from '@fortawesome/free-solid-svg-icons';
-import { faSatelliteDish } from '@fortawesome/free-solid-svg-icons';
-import { faSolarPanel } from '@fortawesome/free-solid-svg-icons';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -19,88 +7,9 @@ import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 })
 export class AppComponent {
   title = 'bitmexcallbot-ui';
-  hidden1 = false;
-  hidden2 = false;
-  hidden3 = false;
-  faCoffee = faCoffee;
-  faAt = faAt;
-  faNewspaper = faNewspaper;
-  faUsers = faUsers;
-  faUser = faUser;
-  faHandshake = faHandshake;
-  faHistory = faHistory;
-  faWallet = faWallet;
-  faCogs = faCogs;
-  faSatelliteDish = faSatelliteDish;
-  faSolarPanel = faSolarPanel;
-  faSignOutAlt = faSignOutAlt;
-  activeNews = false;
-  activeDashboard = false;
-  activeFollow = false;
-  activeFollowers = false;
-  activeTradePanel = true;
-  activeTX = false;
-  activeWallet = false;
-  activeSettings = false;
-  activeAdminPanel = false;
+  activeTab = 'trade';
 
-  showNews() {
-    this.deactivateSidebarTabs();
-    this.activeNews = true;
-  }
-  showDashboard() {
-    this.deactivateSidebarTabs();
-    this.activeDashboard = true;
-  }
-  showFollow() {
-    this.deactivateSidebarTabs();
-    this.activeFollow = true;
-  }
-  showFollowers() {
-    this.deactivateSidebarTabs();
-    this.activeFollowers = true;
-  }
-  showTX() {
-    this.deactivateSidebarTabs();
-    this.activeTX = true;
-  }
-  showWallet() {
-    this.deactivateSidebarTabs();
-    this.activeWallet = true;
-  }
-  showTrade() {
-    this.deactivateSidebarTabs();
-    this.activeTradePanel = true;
-  }
-  showSettings() {
-    this.deactivateSidebarTabs();
-    this.activeSettings = true;
-  }
-  showAdmin() {
-    this.deactivateSidebarTabs();
-    this.activeAdminPanel = true;
-  }
-
-  hideOrAppear1() {
-    this.hidden1 = !this.hidden1;
-  }
-
-  hideOrAppear2() {
-    this.hidden2 = !this.hidden2;
-  }
-
-  hideOrAppear3() {
-    this.hidden3 = !this.hidden3;
-  }
-  deactivateSidebarTabs() {
-    this.activeNews = false;
-    this.activeDashboard = false;
-    this.activeFollow = false;
-    this.activeFollowers = false;
-    this.activeTradePanel = false;
-    this.activeTX = false;
-    this.activeSettings = false;
-    this.activeWallet = false;
-    this.activeAdminPanel = false;
+  onNavigate(tab: string) {
+    this.activeTab = tab;
   }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {
+  faAt,
+  faCoffee,
+  faCogs,
+  faHandshake,
+  faHistory,
+  faSatelliteDish, faSignOutAlt, faSolarPanel,
+  faUser,
+  faUsers,
+  faWallet
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  activeTab = 'trade';
+  @Output() tabSelected = new EventEmitter<string>();
+
+  faCoffee = faCoffee; faAt = faAt; faUsers = faUsers; faSignOutAlt = faSignOutAlt;
+  faHandshake = faHandshake; faHistory = faHistory; faWallet = faWallet; faCogs = faCogs;
+  faSatelliteDish = faSatelliteDish; faSolarPanel = faSolarPanel;  faUser = faUser;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(tab: string) {
+    this.tabSelected.emit(tab);
+    this.activeTab = tab;
+  }
+}
