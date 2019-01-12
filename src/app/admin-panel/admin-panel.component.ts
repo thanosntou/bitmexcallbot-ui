57c6856efc2bc2cbf255682ca79f7246464d1555
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-01-12 22:18:28 +0200

    fix build

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {AdminService} from '../admin.service';
-import {LoginModel} from '../login.model';
-import {HttpClient} from '@angular/common/http';
 import {faSortDown} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,24 +9,14 @@ import {faSortDown} from '@fortawesome/free-solid-svg-icons';
 })
 export class AdminPanelComponent implements OnInit {
   faSortDown = faSortDown;
-  loginList: LoginModel[];
 
-  constructor(private http: HttpClient, private adminService: AdminService) { }
+  constructor(public adminService: AdminService) { }
 
   ngOnInit() {
-    this.loginList = this.adminService.logins;
   }
 
-  // revertUsernameSort() {
-  //   this.loginList = this.adminService.logins.sort(function(a, b) {
-  //     if (a.user.username < b.user.username) { return -1; }
-  //     if (a.user.username > b.user.username) { return 1; }
-  //     return 0;
-  //   }).reverse();
-  // }
-
   revertDateSort() {
-    this.loginList = this.adminService.logins.reverse();
+    this.adminService.logins.reverse();
   }
 
 }
