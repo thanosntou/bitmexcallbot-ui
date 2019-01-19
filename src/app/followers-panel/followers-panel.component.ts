53580d12178c8fe872aa0981cc58af9d2a1697a3
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-01-19 23:25:44 +0200

    followers enable/disable

--- src/app/followers-panel/followers-panel.component.ts
+++ src/app/followers-panel/followers-panel.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { faMinus } from '@fortawesome/free-solid-svg-icons';
+import {faCheckCircle, faMinus} from '@fortawesome/free-solid-svg-icons';
 import {AuthenticationService} from '../authentication.service';
 import {BaseUrl} from '../BaseUrl.enum';
 import {UserModel} from '../user.model';
@@ -23,9 +22,48 @@ export class FollowersPanelComponent implements OnInit {
     const httpOptions = { headers: new HttpHeaders({
         'Authorization': bearerToken,
         'Content-Type': 'application/x-www-form-urlencoded'
-      })};
+    })};
 
    this.http.get<UserModel[]>(BaseUrl.BASEURL + '/api/v1/trader/followers', httpOptions)
      .subscribe((data: UserModel[]) => this.followers = data);
+  }
+
+  onEnable(id: number) {
+    const bearerToken = this.authService.accessToken.token_type + ' ' + this.authService.accessToken.access_token;
+    const httpOptions = { headers: new HttpHeaders({
+        'Authorization': bearerToken,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    })};
+
+    const body = 'followerId=' + id;
+
+    this.http.post<UserModel>(BaseUrl.BASEURL + '/api/v1/trader/status', body, httpOptions)
+      .subscribe((data: UserModel) => {
+        // const fol = this.followers.find(follower => follower.id === data.id);
+        // fol.enabled = data.enabled;
+        // this.followers.push(fol);
+      });
+
+    this.followers.find(follower => follower.id === id).enabled = true;
+  }
+
+  onDisable(id: number) {
+    const bearerToken = this.authService.accessToken.token_type + ' ' + this.authService.accessToken.access_token;
+    const httpOptions = { headers: new HttpHeaders({
+        'Authorization': bearerToken,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    })};
+
+    const body = 'followerId=' + id;
+
+    this.http.post<UserModel>(BaseUrl.BASEURL + '/api/v1/trader/status', body, httpOptions)
+      .subscribe((data: UserModel) => {
+        // const fol = this.followers.find(follower => follower.id === data.id);
+        // fol.enabled = data.enabled;
+        // this.followers.push(fol);
+      });
+
+    this.followers.find(follower => follower.id === id).enabled = false;
+
   }
 }
