b7dce712ccb0124fa8fa3e508d47bdd1898a59d5
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-01-31 20:12:49 +0200

    ref

--- src/app/user-details.model.ts
+++ src/app/user-details.model.ts
@@ -1,20 +1,7 @@
 import {AuthorityModel} from './authority.model';
+import {UserModel} from './user.model';
 
 export class UserDetailsModel {
-  id: number;
-  username: string;
-  email: string;
-  enabled: boolean;
-  apiKey: string;
-  apiSecret: string;
-  fixedQtyXBTUSD: number;
-  fixedQtyETHUSD: number;
-  fixedQtyADAZ18: number;
-  fixedQtyBCHZ18: number;
-  fixedQtyEOSZ18: number;
-  fixedQtyETHZ18: number;
-  fixedQtyLTCZ18: number;
-  fixedQtyTRXZ18: number;
-  fixedQtyXRPZ18: number;
+  user: UserModel;
   authorities: AuthorityModel[];
 }
