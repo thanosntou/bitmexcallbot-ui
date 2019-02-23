f3817985072d2b5368b069261c957ad76a62c1fa
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-02-23 23:02:35 +0200

    cfbfdc

--- src/app/BaseUrl.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum BaseUrl {
-  BASEURL = 'https://bitmexcallbot.herokuapp.com'
-  // BASEURL = 'http://localhost:8082/BioUnion'
-}
---
+++ src/app/_enum/BaseUrl.enum.ts
@@ -0,0 +1,4 @@
+export enum BaseUrl {
+  // BASEURL = 'https://bitmexcallbot.herokuapp.com'
+  BASEURL = 'http://localhost:8082/BioUnion'
+}
