d1b1f3af0ab5e8552794cd0c7029c8d06746b0c5
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-03-24 02:40:16 +0200

    fixed url

--- src/app/_enum/BaseUrl.enum.ts
+++ src/app/_enum/BaseUrl.enum.ts
@@ -1,5 +1,5 @@
 export enum BaseUrl {
   BASEURL = 'https://bitmexcallbot.herokuapp.com',
   // BASEURL = 'http://localhost:8082/BioUnion',
-  B2 = 'https://bitmexcallbot2.herokuapp.com/'
+  B2 = 'https://bitmexcallbot2.herokuapp.com'
 }
